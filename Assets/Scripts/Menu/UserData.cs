74e4eea1f5bdf4dab0315646eefc8035ff01207c
Henrique Ambiel <henriqueambiel@gmail.com>
2025-05-06 21:06:16 -0300

    Editando tela de personalização do usuário
    Adicionando lógica aos botoes e input fields

--- Assets/Scripts/Menu/UserData.cs
+++ Assets/Scripts/Menu/UserData.cs
@@ -1,13 +1,50 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class UserData : MonoBehaviour
 {
     [SerializeField] private string gameplayLevelName;
+    public InputField userNameInput;
+    public Button boyButton;
+    public Button girlButton;
+    public Image boyImage;
+    public Image girlImage;
+
+    private string selectedSkin = "";
+    public static string userName;
+    public static string skin;
+
+    private void Start()
+    {
+        boyButton.onClick.AddListener(() => SelectSkin("Menino"));
+        girlButton.onClick.AddListener(() => SelectSkin("Menina"));
+    }
+
+    void SelectSkin(string skin)
+    {
+        selectedSkin = skin;
+
+        boyImage.gameObject.SetActive(selectedSkin == "Menino");
+        girlImage.gameObject.SetActive(selectedSkin == "Menina");
+    }
+
 
     //Botão que inicia o jogo 
     public void Play()
     {
+        if (string.IsNullOrEmpty(userNameInput.text) || string.IsNullOrEmpty(selectedSkin))
+        {
+            Debug.LogWarning("Preencha todos os campos");
+            return;
+        }
+
+        userName = userNameInput.text;
+        skin = selectedSkin;
+
+        DontDestroyOnLoad(this.gameObject);
+
+
         SceneManager.LoadScene(gameplayLevelName);
         Debug.Log("Jogo Iniciado");
     }
